8e0b7b50aaa77019b770375ad543de606fac8df4
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-05 08:49:35 +0900

    レンダリング最適化②（useCallback）

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { ChildArea } from './components/ChildArea';
 import './App.css';
 
@@ -8,6 +8,7 @@ export default function App() {
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
@@ -15,7 +16,7 @@ export default function App() {
       <br/>
       <br/>
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
