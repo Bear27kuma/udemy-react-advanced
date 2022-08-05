9c0499cdebae4a9dd6a2b7b6535f626265beb6b9
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-05 09:19:58 +0900

    Inline Styles

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,22 +1,11 @@
-import React, { useState, useCallback } from 'react';
-import { ChildArea } from './components/ChildArea';
+import React from 'react';
+import { InlineStyle } from './components/InlineStyle';
 import './App.css';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => setOpen(!open);
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br/>
-      <br/>
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
     </div>
   );
 }
