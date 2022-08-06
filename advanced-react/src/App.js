63cdd5de90700deb065973d7b020d77616c2b504
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-06 20:04:04 +0900

    CSS Modules

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { InlineStyle } from './components/InlineStyle';
+import { CssModules } from './components/CssModules';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
