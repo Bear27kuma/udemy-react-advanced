c007b112f030ba4d227a9fc56e25f00c16f00a67
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-06 20:22:37 +0900

    Styled JSX

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { InlineStyle } from './components/InlineStyle';
 import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
 import './App.css';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
