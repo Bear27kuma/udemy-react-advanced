dd27c7ee2e2f18edbb019500b7bce36a27d557ae
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-10 08:45:46 +0900

    Templateの作成

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { PrimaryButton } from './components/atoms/buttons/PrimaryButton';
 import { SecondaryButton } from './components/atoms/buttons/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import { HeaderOnly } from './components/templates/HeaderOnly';
+import { DefaultLayout } from './components/templates/DefaultLayout';
 import './App.css';
 import './styles.css';
 
@@ -19,13 +23,15 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br/>
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br/>
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
 
