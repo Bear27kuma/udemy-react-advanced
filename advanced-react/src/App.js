03d31f94ebba6b58ed3471f11132713b3543ec85
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-10 09:15:53 +0900

    Pageの作成

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -7,31 +7,11 @@ import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
 import { HeaderOnly } from './components/templates/HeaderOnly';
 import { DefaultLayout } from './components/templates/DefaultLayout';
+import { Router } from './components/router/Router';
 import './App.css';
 import './styles.css';
 
-const user = {
-  name: 'Yuta',
-  image: 'https://source.unsplash.com/NE0XGVKTmcA',
-  email: '1234@example.com',
-  phone: '080-1111-2222',
-  company: {
-    name: 'あいうえお株式会社'
-  },
-  website: 'https://google.com'
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br/>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
 
---
+++ advanced-react/src/components/pages/Users.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { SearchInput } from '../molecules/SearchInput';
+import { UserCard } from '../organisms/user/UserCard';
+
+const users = [...Array(10).keys()].map((key) => {
+  return {
+    id: key,
+    name: `Yuta-${key}`,
+    image: 'https://source.unsplash.com/NE0XGVKTmcA',
+    email: '1234@example.com',
+    phone: '080-1111-2222',
+    company: {
+      name: 'あいうえお株式会社'
+    },
+    website: 'https://google.com'
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput/>
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+  text-align: center;
+`;
+
+const SUserArea = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+  width: 100%;
+  padding-top: 40px;
+`;
